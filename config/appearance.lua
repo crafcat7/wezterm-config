bcb0fb3de2bea0091dba0f8e99bfd3d66f6d8473
crafcat7 <crafcat7@outlook.com>
2024-01-20 18:30:49 +0800

    Remove useless configuration
    Signed-off-by: crafcat7 <crafcat7@outlook.com>

--- config/appearance.lua
+++ config/appearance.lua
@@ -1,11 +1,10 @@
 local wezterm = require('wezterm')
 
 return {
-  font_size = 13,
-  font = wezterm.font("JetBrains Mono NL", { weight = "Regular" }),
   color_scheme = "Catppuccin Mocha" ,
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
   window_decorations = "RESIZE",
   show_new_tab_button_in_tab_bar = false,
   window_background_opacity = 0.9,
